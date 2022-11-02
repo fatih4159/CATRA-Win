21ee2fed8aededbb5b58a42ebc0e1407f4857741
tuluk <fatih4159@gmail.com>
2022-11-02 17:09:47 +0100

    mqtt server hinzugefügt

--- CATRA/MainWindow.xaml.cs
+++ CATRA/MainWindow.xaml.cs
@@ -16,6 +16,13 @@ using System.Threading;
 using System.Net;
 using CATRA.helper;
 using System.Diagnostics;
+using CATRA.MQTT.Broker;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Concurrent;
+using MQTTnet;
+using MQTTnet.Server;
+using MQTTnet.Diagnostics;
 
 namespace CATRA
 {
@@ -29,11 +36,59 @@ namespace CATRA
         public MainWindow()
         {
             InitializeComponent();
-           
-           
+
+
+
 
         }
-       
+        public class ConsoleLogger : IMqttNetLogger
+        {
+            readonly object _consoleSyncRoot = new();
+
+            public bool IsEnabled => true;
+
+            public void Publish(MqttNetLogLevel logLevel, string source, string message, object[]? parameters, Exception? exception)
+            {
+                var foregroundColor = ConsoleColor.White;
+                switch (logLevel)
+                {
+                    case MqttNetLogLevel.Verbose:
+                        foregroundColor = ConsoleColor.White;
+                        break;
+
+                    case MqttNetLogLevel.Info:
+                        foregroundColor = ConsoleColor.Green;
+                        break;
+
+                    case MqttNetLogLevel.Warning:
+                        foregroundColor = ConsoleColor.DarkYellow;
+                        break;
+
+                    case MqttNetLogLevel.Error:
+                        foregroundColor = ConsoleColor.Red;
+                        break;
+                }
+
+                if (parameters?.Length > 0)
+                {
+                    message = string.Format(message, parameters);
+                }
+
+                lock (_consoleSyncRoot)
+                {
+                    //Console.ForegroundColor = foregroundColor;
+                    Debug.WriteLine(message);
+
+
+                    if (exception != null)
+                    {
+                        Debug.WriteLine(message);
+                        //logify(exception.Message);
+                    }
+                }
+            }
+        }
+
 
 
 
@@ -60,6 +115,33 @@ namespace CATRA
         {
             MethodContainer.DoUnProvision(tb_log);
         }
+
+        private async void btn_start_broker_Click(object sender, RoutedEventArgs e)
+        {
+
+            var mqttFactory = new MqttFactory(new ConsoleLogger());
+
+            var mqttServerOptions = new MqttServerOptionsBuilder().WithDefaultEndpoint().WithDefaultEndpointPort(8880).Build();
+            var mqttServer = mqttFactory.CreateMqttServer(mqttServerOptions);
+            await mqttServer.StartAsync();
+
+                //Console.WriteLine("Press Enter to exit.");
+                //Console.ReadLine();
+
+                // Stop and dispose the MQTT server if it is no longer needed!
+                //await mqttServer.StopAsync();
+            
+
+
+        }
+
+        
+
+
+
+
+
+
 
 
     }
